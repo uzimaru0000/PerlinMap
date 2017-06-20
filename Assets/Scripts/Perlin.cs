8a00aca60b462648e8668b407de717e7844366a5
uzimaru <uzimaru0601@gmail.com>
2017-06-20 17:18:31 +0900

    Add Perlin class

--- Assets/Scripts/Perlin.cs
+++ Assets/Scripts/Perlin.cs
@@ -6,71 +6,55 @@ using System;
 
 public class Perlin {
 	// Create a perlin noise
-	public static float[,] CreatePerlin(int w, int h, float scale = 1.0f, Vector2 offs = new Vector2()) {
-		float[,] noise = new float[w, h];
+	public static float[] CreatePerlin(int w, int h, float scale = 1.0f, Vector2 offs = new Vector2()) {
+		float[] noise = new float[w * h];
 		for (int x = 0; x < w; x++) {
 			for (int y = 0; y < h; y++) {
 				var dx = offs.x + (float)x / w * scale;
 				var dy = offs.y + (float)y / h * scale;
-				noise[x, y] = Mathf.PerlinNoise(dx, dy);
+				noise[x + y * w] = Mathf.PerlinNoise(dx, dy);
 			}
 		}
 		return noise;
 	}
 
 	// Combert array to texture
-	public static Texture2D CreateTexture(float[,] array) {
-		int w = array.GetLength(0),
-			h = array.GetLength(1);
+	public static Texture2D CreateTexture(float[] array, int w, int h) {
 		Texture2D tex = new Texture2D(w, h);
-		Color[] col = new Color[w * h];
-		for (int x = 0; x < w; x++) {
-			for (int y = 0; y < h; y++) {
-				col[x + y * w] = new Color(array[x, y], array[x, y], array[x, y]);
-			}
+		Color[] col = new Color[array.Length];
+		for (int i = 0; i < array.Length; i++) {
+			col[i] = new Color(array[i], array[i], array[i]);
 		}
 		tex.SetPixels(col);
 		tex.Apply();
 		return tex;
 	} 
 
-	public static float[,] CreateMap(int w, int h, Vector2 offs, float amplitude, int octave = 1) {
-		var baseTex = Array2List<float>(CreatePerlin(w, h, 1, offs));
-		System.Func<List<List<float>>, int, List<List<float>>> func = null;
-		func = (bt, n) => {
-			if (n == 0) {
-				return bt;
+	public static float[] CreateMap(int w, int h, Vector2 offs, float amplitude, float scale = 1, int octave = 1) {
+		var baseTex = CreatePerlin(w, h, scale, offs);
+		System.Action<int> func = null;
+		func = (n) => {
+			if (n <= 0) {
+				return;
 			} else {
-				var tex = Array2List<float>(CreatePerlin(w, h, Mathf.Pow(2, n), offs));
-				return func(
-					baseTex.Select(x => x.Join());
-				);
+				var tex = CreatePerlin(w, h, Mathf.Pow(2, n), offs);
+				for (var i = 0; i < baseTex.Length; i++) {
+					baseTex[i] *= tex[i] * amplitude / n;
+				}
 			}
 		};
-
-		return func(baseTex, octave);
+		func(octave);
+		var max = baseTex.Max();
+		return baseTex.Select(x => x / max).ToArray();
 	}
 
-	static List<List<T>> Array2List<T>(T[,] arr) {
-		var list = new List<List<T>>();
-		for (int x = 0; x < arr.GetLength(0); x++) {
-			list.Add(new List<T>());
-			for (int y = 0; y < arr.GetLength(1); y++) {
-				list[x].Add(arr[x, y]);
+	public static float[,] ConvertArray(float[] array, int w, int h) {
+		var tex = new float[w, h];
+		for (var x = 0; x < w; x++) {
+			for (var y = 0; y < h; y++) {
+				tex[x, y] = array[x + y * w];
 			}
 		}
-		return list;
-	}
-
-	static T[,] List2Array<T>(List<List<T>> list) {
-		var arr = list.ToArray();
-		var result = new T[list.Count, list[0].Count];
-		for (int i = 0; i < arr.GetLength(0); i++) {
-			var a = arr[i].ToArray();
-			for (int j = 0; j < a.Length; j++) {
-				result[i, j] = a[j];
-			}
-		}
-		return result;
+		return tex;
 	}
 }
--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -6,10 +6,17 @@ public class Sample : MonoBehaviour {
 
 	public float scale;
 	public Vector2 offs;
+	public int octave;
+
+	Terrain terrain;
 
 	// Use this for initialization
 	void Start () {
-		
+		terrain = GetComponent<Terrain>();
+		var data = terrain.terrainData;
+		var w = data.heightmapWidth;
+		var h = data.heightmapHeight;
+		data.SetHeights(0, 0, Perlin.ConvertArray(Perlin.CreateMap(w, h, offs, 1, scale, octave), w, h));
 	}
 	
 	// Update is called once per frame
@@ -18,6 +25,7 @@ public class Sample : MonoBehaviour {
 	}
 
 	void OnDrawGizmos() {
-		Perlin.CreateMap(32, 32, offs, 1);
+		var tex = Perlin.CreateTexture(Perlin.CreateMap(128, 128, offs, 1, scale, octave), 128, 128);
+		Gizmos.DrawGUITexture(new Rect(0, 0, 128, 128), tex);
 	}
 }
